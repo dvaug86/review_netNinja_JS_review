12b8a32985e0b3cda3b480b41ee65ed85c762523
David Vaughan <dvaug86@gmail.com>
2024-03-08 13:01:13 -0500

    if statements

---
+++ ifStatements.js
@@ -0,0 +1,25 @@
+// var youLikeMeat = true;
+// var youLikeMeat = false
+// if (youLikeMeat){ //area between curly brackets is a code block
+
+// if (7 > 5) {//inside the parenthesis here can be any kind of parameters
+
+//   document.write("hear is the meaty menu...");
+
+// }
+
+
+var myNum = 10;
+if(myNum>10){
+    document.write("myNum is greater thn 10");
+
+//nothing will be printed here because 10 is not greater than 10 it is equal. Ifmy num was less than 10
+//or if the statement said (myNum ==10 ) or (myNum <=10) or  (myNum <=10) then the statement would be
+//printed
+
+
+}else{ //the else statement comes in if the "if statement" parameter is false and will excute the followig
+        //block of code
+
+document.write("my numb is not greater than 10");
+}
