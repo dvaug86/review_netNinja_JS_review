6c8fc8e971e07f18bd9556faf9b60814b5ec9b46
David Vaughan <dvaug86@gmail.com>
2024-03-08 15:47:24 -0500

    logical operators

---
+++ logicalOperators.js
@@ -0,0 +1,17 @@
+var myAge = 25;
+//this is and &&
+// if (myAge >= 18 && myAge <= 30) {
+//     document.write("you can come");
+// }else{
+
+//     document.write("you can't come");
+
+// }
+
+//this is the or ||
+if (myAge < 18 || myAge > 31) {
+  document.write("you can't come");
+} else {
+  document.write("you can come");
+}
+
