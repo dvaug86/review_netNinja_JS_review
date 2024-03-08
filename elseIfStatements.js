16442ab432567d8ec69b7fc03ca1b4fd7735dc8c
David Vaughan <dvaug86@gmail.com>
2024-03-08 15:27:42 -0500

    else if statement

---
+++ elseIfStatements.js
@@ -0,0 +1,13 @@
+//else if statements check for multiple different conditions.
+
+var myAge = 5;
+
+if(myAge >30){
+    document.write("you are over 30");
+}else if(myAge > 20){
+    document.write("you are over 20");
+}else if(myAge > 10){
+    document.write("you are over 10");
+}else{
+    document.write("you are not over 10")
+}
