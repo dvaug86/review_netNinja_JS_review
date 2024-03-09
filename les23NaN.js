6ae5651ad2f1f3bbe4b6570100dcaf998d84e26c
David Vaughan <dvaug86@gmail.com>
2024-03-08 19:53:27 -0500

    strings

---
+++ les23NaN.js
@@ -0,0 +1,18 @@
+// var a = "apple";
+var a = 6;
+var b = 5;
+
+// if(isNaN(a)){
+//     console.log("that isnt a number");
+// }else {
+//     console.log("the product is " (a * b));
+// }
+
+
+
+if(!isNaN(a)){
+    console.log("the product is " + (a * b));
+    
+}else {
+    console.log("that isnt a number");
+}
---
+++ les24Strings.js
@@ -0,0 +1,24 @@
+// var myString = "I'm a \"fun\" string ";
+
+// console.log(myString);
+// console.log(myString.length); //length is a property
+// console.log(myString.toUpperCase()); //method
+// console.log(myString.indexOf("string")); //method the back slashes dont count since they are escape characters
+// //-1 will be given if string doesn't exist
+// //there are also methods
+//  if (myString.indexOf("ninja")===-1){
+//     console.log("the word ninja is not in the string")
+//  }
+
+
+//strings are case sensitive
+ var string1 = "abc";
+ var string2 = "bcd";
+
+ console.log(string1 < string2) //this is true because a is before b in the alphabet
+
+ var string3 = "abc";
+ var string4 = "Bcd";
+
+ console.log(string3 < string4) //this is true because a is before b in the alphabet
+ //therefore this will be false
