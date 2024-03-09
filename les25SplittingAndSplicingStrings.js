289771fda65d8f38eba15f67db323a587b64050c
David Vaughan <dvaug86@gmail.com>
2024-03-08 20:05:08 -0500

    splitting and slicing

---
+++ les25SplittingAndSplicingStrings.js
@@ -0,0 +1,11 @@
+var str = "hello, world";
+console.log(str)
+
+var str2 =str.slice(2,9) //does not include last element in this case index 9 is r but we will get "llo wo"
+console.log(str2)
+
+var tags = "meat, ham, salami, pork, beef, chicken"
+console.log(tags)
+
+var tagsArray =tags.split(",")
+console.log(tagsArray)
