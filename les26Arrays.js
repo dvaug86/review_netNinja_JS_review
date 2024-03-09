ce1c67a9af19b4f1641957bcdc455cdc95673d7b
David Vaughan <dvaug86@gmail.com>
2024-03-09 09:32:03 -0500

    arrays

---
+++ les26Arrays.js
@@ -0,0 +1,21 @@
+var myArray = [];
+
+myArray[0] = 25;
+//this will make the value of index 0, 25
+//this is also how you can update values
+console.log(myArray);
+
+
+myArray2=[1, 2, 3, 4, 5, 6];
+console.log(myArray2);
+
+//another way to make arrays is:
+var myArray3 = new Array();
+
+//inorder to determine how man slots
+//this will have 5 slots
+var myArray4 = new Array(5);
+console.log("array 4 " + myArray4);
+myArray4[3] = "maybe";
+console.log(myArray4);
+
